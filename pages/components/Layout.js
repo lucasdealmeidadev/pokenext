ff84e4e4b6e3f71eb6414ff0ea76b9afbd738eb9
Lucas de Almeida <lucasdealmeida.monteiro@gmail.com>
2022-04-21 15:27:52 -0300

    Criando componente de layout

---
+++ pages/components/Layout.js
@@ -0,0 +1,20 @@
+import { Fragment } from 'react';
+import Head from 'next/head';
+import Footer from './Footer';
+import Navbar from './Navbar';
+
+export default function Layout({ children }) {
+    return (
+        <Fragment>
+            <Head>
+                <link rel='shortcut icon' href='/images/favicon.ico' />
+                <title>PokeNet</title>
+            </Head>
+            <Navbar />
+            <main>
+                {children}
+            </main>
+            <Footer />
+        </Fragment>
+    );
+}
